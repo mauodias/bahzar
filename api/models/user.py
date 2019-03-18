1098a58dba9ef61d8649e1d00594f5a5ef8badf4
Maurício Dias <mau.dias@gmail.com>
2019-03-18 01:31:26 -0300

    Adiciona método init_with_document no modelo users

--- api/models/user.py
+++ api/models/user.py
@@ -2,7 +2,15 @@ from .db import get_connection
 from .model import Model
 
 class User(Model):
-  connection = get_connection()['users']
+  collection = get_connection()['users']
 
   def __init__(self, email):
     self.email = email
+
+  @staticmethod
+  def init_with_document(document):
+    _id = document.get('_id', None)
+    email = document.get('email', None)
+    result = User(email=email)
+    result._id = _id
+    return result
