b697ae101ce43854124cbbd1b33564711f496586
Maurício Dias <mau.dias@gmail.com>
2019-03-18 01:32:05 -0300

    Adiciona tags, remove metodos get

--- api/models/product.py
+++ api/models/product.py
@@ -1,48 +1,36 @@
 from .db import get_connection
 from .model import Model
+from bson import ObjectId
 
 class Product(Model):
   collection = get_connection()['products']  
 
-  def __init__(self, owner, name, price, description, _id=None, available=True, images=[]):
-    self._id = _id
+  def __init__(self, owner, name, price, tags, description, available=True, images=[]):
     self.owner = owner
     self.name = name
     self.price = price
+    self.tags = tags
     self.description = description
     self.available = available
     self.images = images
 
   def sell(self):
     self.available = False
-    return True
 
   def insert_images(self, images):
     for image in images:
       self.images.append(image)
 
-  @classmethod
-  def init_with_document(cls, document):
+  @staticmethod
+  def init_with_document(document):
     _id = document.get('_id', None)
     owner = document.get('owner', None)
     name = document.get('name', None)
     price = document.get('price', None)
+    tags = document.get('tags', None)
     description = document.get('description', None)
-    available = document.get('available', None)
-    images = document.get('images', None)
-    return cls(_id=_id, owner=owner, name=name, price=price, description=description, available=available, images=images)
-
-  @classmethod
-  def get_products(cls, owner=None):
-    results = []
-    for product in cls.collection.find({'owner': owner} if owner else {}):
-      results.append(cls.init_with_document(product))
-    return results
-
-  @classmethod
-  def get_product(cls, _id):
-    result = cls.collection.find_one({'_id': _id})
-    if result:
-      return cls.init_with_document(result)
-    else:
-      return None
+    available = document.get('available', True)
+    images = document.get('images', [])
+    result = Product(owner=owner, name=name, price=price, tags=tags, description=description, available=available, images=images)
+    result._id = _id
+    return result 
